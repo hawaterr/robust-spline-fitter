0ea84fdd5ea9548adc665c2bb3617d31fb842475
Ali <ali.hawater@gmail.com>
2026-08-04 01:49:01 +0200

    updated run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,4 @@ repo_root="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cmake -S "$repo_root" -B "$repo_root/build"
 cmake --build "$repo_root/build"
 
-cd "$repo_root/build"
-./fit_spline
-python3 "$repo_root/scripts/plot_result.py"
+python3 "$repo_root/main.py"
